a2fac482f085608b6d496fedd0606503e0ac486f
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-05-15 19:41:36 -0500

    Configured DbContext

---
+++ EntityFrameworkDemo.Business/Context/SubSystemDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EntityFrameworkDemo.Business.Connection;
+using EntityFrameworkDemo.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkDemo.Business.Context
+{
+    public class SubSystemDbContext : DbContext
+    {
+        DbSet<Device> Devices { get; set; }
+        DbSet<SubSystem> SubSystems { get; set; }
+
+        public SubSystemDbContext() { }
+
+        public SubSystemDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Hidden.GetConnectionString());
+            }
+        }
+    }
+}
