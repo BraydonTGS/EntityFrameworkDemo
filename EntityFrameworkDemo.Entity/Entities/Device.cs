b41c1d4fa6aa50c9b542bceea0a099e9417e296e
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-05-15 19:29:20 -0500

    Created Device and SubSystem Entity Objects

---
+++ EntityFrameworkDemo.Entity/Entities/Device.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFrameworkDemo.Entity.Entities
+{
+    public class Device
+    {
+        [Key]
+        [Required]
+        [Column("Id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("Description")]
+        public string? Description { get; set; }
+
+        [ForeignKey("SubSystemId")]
+        [Column("SubSystemId")]
+        public int SubSystemId { get; set; }
+        public virtual SubSystem? SubSystem { get; set; }
+    }
+}
---
+++ EntityFrameworkDemo.Entity/Entities/SubSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkDemo.Entity.Entities
+{
+    public class SubSystem
+    {
+        [Key]
+        [Required]
+        [Column("Id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("Description")]
+        public string? Description { get; set; }
+
+        public virtual ICollection<Device>? Devices { get; set; }
+    }
+}
