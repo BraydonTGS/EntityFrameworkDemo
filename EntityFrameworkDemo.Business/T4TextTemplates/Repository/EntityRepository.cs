f33eb4581dcce328691ed1238e1d73f71c53ec0f
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-05-22 17:56:33 -0500

    Updated T4 Text Template and Re Generated Partial Repos

---
+++ EntityFrameworkDemo.Business/T4TextTemplates/Repository/EntityRepository.cs
@@ -0,0 +1,39 @@
+﻿using EntityFrameworkDemo.Business.Base;
+using EntityFrameworkDemo.Business.Context;
+using EntityFrameworkDemo.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+//------------------------------------------------------------
+//
+//
+// THIS CODE IS AUTO GENERATED. DO NOT MAKE MANUAL CHANGES!!!
+//
+//
+//------------------------------------------------------------
+namespace EntityFrameworkDemo.Business.T4TextTemplates.Generated
+{
+	#region SubSystemRepository
+	public partial class SubSystemRepository : BaseRepository<SubSystem>
+	{
+		private readonly IDbContextFactory<SubSystemDbContext> _contextFactory;
+
+		public SubSystemRepository(IDbContextFactory<SubSystemDbContext> contextFactory) : base(contextFactory)
+		{
+			_contextFactory = contextFactory;
+		}
+	}
+	#endregion
+
+	#region DeviceRepository
+	public partial class DeviceRepository : BaseRepository<Device>
+	{
+		private readonly IDbContextFactory<SubSystemDbContext> _contextFactory;
+
+		public DeviceRepository(IDbContextFactory<SubSystemDbContext> contextFactory) : base(contextFactory)
+		{
+			_contextFactory = contextFactory;
+		}
+	}
+	#endregion
+
+}
