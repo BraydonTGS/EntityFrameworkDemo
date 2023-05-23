702586df9e89151b8cd5d26cb3db4fad9eff2539
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-05-22 19:24:52 -0500

    Created Device and System Dto Validator

---
+++ EntityFrameworkDemo.Business/Validations/Dto/DeviceDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameworkDemo.Business.Context;
+using EntityFrameworkDemo.Business.Dto;
+using EntityFrameworkDemo.Business.Validations.Extensions;
+using EntityFrameworkDemo.Entity.Entities;
+using FluentValidation;
+
+namespace EntityFrameworkDemo.Business.Validations.Dto
+{
+    public class DeviceDtoValidator : AbstractValidator<DeviceDto>
+    {
+        private readonly IDbContextValidationHelper<SubSystemDbContext> _validationHelper;
+        public DeviceDtoValidator(IDbContextValidationHelper<SubSystemDbContext> validationHelper)
+        {
+            _validationHelper = validationHelper;
+            RuleFor(x => x).MustAsync((x, cancellation) => _validationHelper.DoesEntityExistWithEntity<Device>(d => d.Id == x.Id && d.SubSystemId == x.SubSystemId)); 
+        }
+    }
+}
---
+++ EntityFrameworkDemo.Business/Validations/Dto/SubSystemDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkDemo.Business.Context;
+using EntityFrameworkDemo.Business.Validations.Extensions;
+using EntityFrameworkDemo.Entity.Entities;
+using FluentValidation;
+
+namespace EntityFrameworkDemo.Business.Validations.Dto
+{
+    public class SubSystemDtoValidator : AbstractValidator<SubSystem>
+    {
+        private readonly IDbContextValidationHelper<SubSystemDbContext> _validationHelper;
+
+        public SubSystemDtoValidator(IDbContextValidationHelper<SubSystemDbContext> validationHelper)
+        {
+            _validationHelper = validationHelper;
+        }
+    }
+}
