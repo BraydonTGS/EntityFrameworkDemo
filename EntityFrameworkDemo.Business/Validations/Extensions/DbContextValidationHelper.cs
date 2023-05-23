002d69c38e6122326a12547d6922cfa869f24914
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-05-22 19:24:27 -0500

    Created DbContextValidationHelper

---
+++ EntityFrameworkDemo.Business/Validations/Extensions/DbContextValidationHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EntityFrameworkDemo.Business.Validations.Extensions
+{
+    public class DbContextValidationHelper<TContext> where TContext : DbContext, IDbContextValidationHelper<TContext>
+    {
+        private readonly TContext _context;
+
+        public DbContextValidationHelper(TContext context)
+        {
+            _context = context;
+        }
+
+        #region DoesExist
+        public async Task<bool> DoesExist<TEntity>(Expression<Func<TEntity, bool>> exp) where TEntity : class
+        {
+            var entity = _context.Set<TEntity>();
+            var result = await entity.AnyAsync(exp);
+            return result;
+        }
+        #endregion
+
+        #region DoesEntityExistWithEntity
+        public async Task<bool> DoesEntityExistWithEntity<TEntity>(Expression<Func<TEntity, bool>> exp) where TEntity : class
+        {
+            var entity = _context.Set<TEntity>();
+            var result = await entity.AnyAsync(exp);
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ EntityFrameworkDemo.Business/Validations/Extensions/IDbContextValidationHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EntityFrameworkDemo.Business.Validations.Extensions
+{
+    public interface IDbContextValidationHelper<TContext> where TContext : DbContext
+    {
+        public Task<bool> DoesExist<TEntity>(Expression<Func<TEntity, bool>> exp) where TEntity : class;
+        public Task<bool> DoesEntityExistWithEntity<TEntity>(Expression<Func<TEntity, bool>> exp) where TEntity : class; 
+    }
+}
