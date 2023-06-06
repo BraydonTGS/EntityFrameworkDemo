f2f47509b5697e1a770ba27cc53a12b9e10e1385
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-06-06 05:51:39 -0500

    Registered Services in WPF App.cs

--- EntityFrameworkDemo.WPF/App.xaml.cs
+++ EntityFrameworkDemo.WPF/App.xaml.cs
@@ -1,6 +1,36 @@
-﻿namespace EntityFrameworkDemo.WPF
+﻿using EntityFrameworkDemo.Business.Config;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+
+namespace EntityFrameworkDemo.WPF
 {
     public partial class App : Application
     {
+        private static IServiceProvider? ServiceProvider { get; set; }
+
+        public App()
+        {
+            // Register services
+            StartUp();
+
+            // Other initialization code
+        }
+
+        private void StartUp()
+        {
+            // Create an instance of the RegisterServices class
+            var serviceConfig = new RegisterServices();
+
+            // Get the IServiceCollection from the RegisterServices class
+            var services = serviceConfig.ConfigureServices();
+
+            // Build the service provider
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Set the application's service provider
+            // This will allow you to resolve services throughout the application
+            ServiceProvider = serviceProvider;
+        }
     }
 }
