b93a226704679f3d3eb63ff3d2746bbe529e5422
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-06-12 19:43:01 -0500

    Migration Added Successfully

--- EntityFrameworkDemo.Business/Migrations/20230613003943_AddedUserAndPassword.Designer.cs
+++ EntityFrameworkDemo.Business/Migrations/20230613003943_AddedUserAndPassword.Designer.cs
@@ -3,6 +3,7 @@ using EntityFrameworkDemo.Business.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityFrameworkDemo.Business.Migrations
 {
     [DbContext(typeof(SubSystemDbContext))]
-    partial class SubSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230613003943_AddedUserAndPassword")]
+    partial class AddedUserAndPassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EntityFrameworkDemo.Business/Migrations/20230613003943_AddedUserAndPassword.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkDemo.Business.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserAndPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Password",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Hash = table.Column<byte[]>(type: "varbinary(100)", maxLength: 100, nullable: false),
+                    Salt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Password", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Password_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Password_UserId",
+                table: "Password",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName_Email",
+                table: "Users",
+                columns: new[] { "UserName", "Email" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Password");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
