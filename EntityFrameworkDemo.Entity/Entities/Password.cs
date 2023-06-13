e5252a8095f4d8749a47638b8e64fc4e9c438763
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-06-12 19:34:44 -0500

    Created User and Password Entity

---
+++ EntityFrameworkDemo.Entity/Entities/Password.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkDemo.Entity.Entities
+{
+    public class Password
+    {
+        [Key]
+        [Required]
+        [Column("Id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Hash")]
+        [MaxLength(100)]
+        public byte[] Hash { get; set; } = new byte[0];
+
+        [Required]
+        [Column("Salt")]
+        public byte[] Salt { get; set; } = new byte[0];
+
+        public int UserId { get; set; }
+
+        public virtual User? User { get; set; } 
+    }
+}
---
+++ EntityFrameworkDemo.Entity/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFrameworkDemo.Entity.Entities
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [Column("Id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [Column("LastName")]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [Column("Email")]
+        [MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Column("UserName")]
+        [MaxLength(25)]
+        public string UserName { get; set; } = string.Empty;
+
+    }
+}
