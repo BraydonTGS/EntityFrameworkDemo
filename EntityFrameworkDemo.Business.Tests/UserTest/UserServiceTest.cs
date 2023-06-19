bbee7ac88826b0f1608041a101e9c474479f03dd
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-06-19 18:53:09 -0500

    Building UserServiceTest Class

--- EntityFrameworkDemo.Business.Tests/UserTest/UserServiceTest.cs
+++ EntityFrameworkDemo.Business.Tests/UserTest/UserServiceTest.cs
@@ -1,6 +1,22 @@
-﻿namespace EntityFrameworkDemo.Business.Tests.UserTest
+﻿using EntityFrameworkDemo.Business.Interfaces;
+using EntityFrameworkDemo.Business.Tests.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EntityFrameworkDemo.Business.Tests.UserTest
 {
-    public class UserServiceTest
+    [TestClass]
+    public class UserServiceTest : TestBase
     {
+        private readonly IServiceCollection _services;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IUserService _service;
+        private readonly DatabaseSeeder _databaseSeeder;
+        public UserServiceTest()
+        {
+            _services = ConfigureServices(seedDatabase: true);
+            _serviceProvider = _services.BuildServiceProvider();
+            _service = _serviceProvider.GetRequiredService<IUserService>();
+            _databaseSeeder = _serviceProvider.GetRequiredService<DatabaseSeeder>();
+        }
     }
 }
