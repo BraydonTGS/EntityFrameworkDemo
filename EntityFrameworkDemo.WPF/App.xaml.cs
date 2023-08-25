a7519c7452d6bfa297f44f5d97f7eaf81db9d0bb
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-08-24 19:40:51 -0500

    Created a Test Module and Added it to the Module Catalog

--- EntityFrameworkDemo.WPF/App.xaml.cs
+++ EntityFrameworkDemo.WPF/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using EntityFrameworkDemo.WPF.Core.Regions;
 using EntityFrameworkDemo.WPF.Views;
+using ModuleA;
 using Prism.DryIoc;
 using Prism.Ioc;
+using Prism.Modularity;
 using Prism.Regions;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,10 +22,23 @@ namespace EntityFrameworkDemo.WPF
             return Container.Resolve<MainWindow>();
         }
 
+        /// <summary>
+        /// Configure Custom Region Adapters and Set Mappings
+        /// </summary>
+        /// <param name="regionAdapterMappings"></param>
         protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
         {
             base.ConfigureRegionAdapterMappings(regionAdapterMappings);
             regionAdapterMappings.RegisterMapping(typeof(StackPanel), Container.Resolve<StackPanelRegionAdapter>());
+        }
+
+        /// <summary>
+        /// Add Modules to the Module Catalog
+        /// </summary>
+        /// <param name="moduleCatalog"></param>
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleAModule>();
         }
     }
 }
